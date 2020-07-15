05fe2d072aeae7bd981980a136c9f0b65423666f
Bruce Cantarim <bruce@cantarim.com>
2020-07-15 04:23:53 -0300

    feat(Hero): implementation and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import "./App.scss";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import './App.scss';
 
 const App = () => (
-  <div className="App">
+  <div className='App'>
     <Navbar />
+    <Hero />
   </div>
 );
 
